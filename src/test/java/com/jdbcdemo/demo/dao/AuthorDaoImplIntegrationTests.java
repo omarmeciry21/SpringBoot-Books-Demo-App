3d70df05ce104d358636cae4e74040ec936c46b8
Omar Nabil ElMesiry <61906752+omarmeciry21@users.noreply.github.com>
2026-08-08 07:33:18 +0300

    add sql initialization mode to application properties and enhance integration tests for AuthorDaoImpl and BookDaoImpl

--- src/test/java/com/jdbcdemo/demo/dao/AuthorDaoImplIntegrationTests.java
+++ src/test/java/com/jdbcdemo/demo/dao/AuthorDaoImplIntegrationTests.java
@@ -7,14 +7,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class AuthorDaoImplIntegrationTests {
 
     private final AuthorDaoImpl underTest;
@@ -31,5 +35,22 @@ public class AuthorDaoImplIntegrationTests {
         Optional<Author> result = underTest.find(author.getId());
         assertThat(result).isPresent();
         assertThat(result.get()).isEqualTo(author);
+    }
+
+    @Test
+    public void testThatMultipleAuthorsCanBeCreatedAndRecalled(){
+
+        Author authorA = TestUtils.createTestAuthorA();
+        Author authorB = TestUtils.createTestAuthorB();
+        Author authorC = TestUtils.createTestAuthorC();
+
+        underTest.create(authorA);
+        underTest.create(authorB);
+        underTest.create(authorC);
+
+        List<Author> result = underTest.findAll();
+        assertThat(result)
+                .hasSize(3)
+                .containsExactly(authorA,authorB,authorC);
     }
 }
--- src/test/java/com/jdbcdemo/demo/dao/BookDaoImplIntegrationTests.java
+++ src/test/java/com/jdbcdemo/demo/dao/BookDaoImplIntegrationTests.java
@@ -9,14 +9,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class BookDaoImplIntegrationTests {
 
     BookDaoImpl underTest;
@@ -42,5 +46,25 @@ public class BookDaoImplIntegrationTests {
         assertThat(result).isPresent();
         assertThat(result.get()).isEqualTo(book);
 
+    }
+
+    @Test
+    public void testThatMultipleBooksCanBeCreatedAndRecalled(){
+        Author author = TestUtils.createTestAuthorA();
+        authorDao.create(author);
+
+        Book bookA = TestUtils.createTestBookA();
+        Book bookB = TestUtils.createTestBookB();
+        Book bookC = TestUtils.createTestBookC();
+
+        underTest.create(bookA);
+        underTest.create(bookB);
+        underTest.create(bookC);
+
+        List<Book> result = underTest.findAll();
+
+        assertThat(result)
+                .hasSize(3)
+                .containsExactly(bookA,bookB,bookC);
     }
 }
